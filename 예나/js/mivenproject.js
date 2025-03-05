918f3b43971c558760b07300378926685350033c
연아윤 <w2306@e-mirim.hs.kr>
2025-03-05 23:45:35 +0900

    Clean up untracked files

--- 예나/js/mivenproject.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const video = document.getElementById('video');
-
-  video.addEventListener('play', function () {
-      setTimeout(function () {
-          video.classList.add('fadeOut');
-      }, 5000); 
-  });
-});
